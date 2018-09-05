0c331d5d7a11bfd55a3a15e0670de18420c763ce
StormPhoenix <hzau.cs.wangcheng@gmail.com>
2018-09-05 23:14:29 +0800

    修改README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# 华农BBS
+
+## introduction
+华农官方BBS后端
+
+## framework
+Spring boot + MyBatis
+
+## start up
+运行 HzauBbsWebApplication.main() 
---
+++ hzau-bbs-web/src/main/java/org/feidian/hzau/bbs/web/HzauBbsWebApplication.java
@@ -0,0 +1,12 @@
+package org.feidian.hzau.bbs.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzauBbsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzauBbsWebApplication.class, args);
+    }
+}
